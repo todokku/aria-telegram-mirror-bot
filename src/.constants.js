5eb44b14159aa741625dc136f5aebf4adeab6a96
viperadnan-git <60497650+viperadnan-git@users.noreply.github.com>
2020-02-02 14:20:44 +0530

    Update and rename .constants.js.example to .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,21 +1,21 @@
 module.exports = Object.freeze({
-  TOKEN: 'bot_token',
+  TOKEN: '1088945005:AAG2RwZOVNVpIHJA2lNOo1hv9-MkDObkPjs',
   ARIA_SECRET: 'aria2_secret',
   ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
   ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  GDRIVE_PARENT_DIR_ID: 'id_of_Drive_folder_to_upload_into',
-  SUDO_USERS: [012, 345],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [678, 901],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  GDRIVE_PARENT_DIR_ID: '1DV1ZCf3H2gSYZTUEWZY-KoLT7Wv_bE9m',
+  SUDO_USERS: [939425014, 999197022],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [-1001384052869, -1001373733175],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
     ENABLED: false,
-    EMAILS: ['someMail@gmail.com', 'someOtherMail@gmail.com']
+    EMAILS: ['nopeyoui@gmail.com', 'someOtherMail@gmail.com']
   },
   DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
-    enabled: false,   // Set this to true to use the notify functionality
+    enabled: true,   // Set this to true to use the notify functionality
     host: 'hostname.domain',
     port: 80,
     path: '/botNotify'
