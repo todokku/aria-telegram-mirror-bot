0a081e0f88e209a43a27a4a4aefff6e7f5137c06
viperadnan-git <60497650+viperadnan-git@users.noreply.github.com>
2020-02-03 11:59:45 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
   TOKEN: '1088945005:AAG2RwZOVNVpIHJA2lNOo1hv9-MkDObkPjs',
-  ARIA_SECRET: '786786',
+  ARIA_SECRET: '13345',
   ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
   ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
@@ -9,13 +9,13 @@ module.exports = Object.freeze({
   GDRIVE_PARENT_DIR_ID: '1DV1ZCf3H2gSYZTUEWZY-KoLT7Wv_bE9m',
   SUDO_USERS: [939425014, 999197022],	// Telegram user IDs. These users can use the bot in any chat.
   AUTHORIZED_CHATS: [-1001384052869, -1001373733175],	// Telegram chat IDs. Anyone in these chats can use the bot.
-  STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
+  STATUS_UPDATE_INTERVAL_MS: 1200, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
     ENABLED: false,
     EMAILS: ['nopeyoui@gmail.com', 'someOtherMail@gmail.com']
   },
   DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
-    enabled: true,   // Set this to true to use the notify functionality
+    enabled: false,   // Set this to true to use the notify functionality
     host: 'hostname.domain',
     port: 80,
     path: '/botNotify'
